547697033db8ea989c979a58fd09855ba0f6f54b
AnjanaR360 <anjanarusiru360@gmail.com>
2025-03-03 16:40:30 +0530

    add comment bar and send

--- lib/features/community/presentation/pages/comment_page.dart
+++ lib/features/community/presentation/pages/comment_page.dart
@@ -1,8 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:furrpal/constant/constant.dart';
 
-class CommentPage extends StatelessWidget {
+class CommentPage extends StatefulWidget {
   const CommentPage({super.key});
+
+  @override
+  _CommentPageState createState() => _CommentPageState();
+}
+
+class _CommentPageState extends State<CommentPage> {
+  final TextEditingController _commentController = TextEditingController();
+  final List<String> _comments = [];
+
+  void _addComment() {
+    if (_commentController.text.isNotEmpty) {
+      setState(() {
+        _comments.add(_commentController.text);
+        _commentController.clear();
+      });
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Please enter a comment")),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,6 +34,42 @@ class CommentPage extends StatelessWidget {
           'Comments',
           style: appBarStyle,
         ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: _comments.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(_comments[index]),
+                  leading: Icon(Icons.person),
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _commentController,
+                    decoration: InputDecoration(
+                      hintText: "Write a comment...",
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 10),
+                IconButton(
+                  icon: Icon(Icons.send, color: Colors.blue),
+                  onPressed: _addComment,
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
